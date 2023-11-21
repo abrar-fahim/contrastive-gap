41ddd6e9d0fa9554b243b7a5c3529ac49657be8e
Abrar Fahim <abrar9597@gmail.com>
2023-11-20 18:49:31 -0700

    fixed csv name bug

--- src/utils.py
+++ src/utils.py
@@ -350,9 +350,9 @@ def generate_csv_file_name(clip_model):
         elif 'loss' in part:
 
             if training_hyperparameters['intra_modality_loss']:
-                new_part = part.replace('loss', str(training_hyperparameters['Lit']))
+                new_part = part.replace('loss', 'Lit')
             else:
-                new_part = part.replace('loss', str(training_hyperparameters['Lit_ii_tt']))
+                new_part = part.replace('loss', 'Lit_ii_tt')
         else:
             new_part = part
 
